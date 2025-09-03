073c6fa48803aaaf7fc6e7272d986fbedd1f6a3f
JPSSmaths <joaopedrosilvadasilva4@gmail.com>
2025-09-03 18:15:02 -0300

    CompanyEntity finalization with restrictions to the fields and ErrorMessageDTO creation

--- src/main/java/br/com/gestao_cursos/modules/company/Entity/CompanyEntity.java
+++ src/main/java/br/com/gestao_cursos/modules/company/Entity/CompanyEntity.java
@@ -3,10 +3,15 @@ package br.com.gestao_cursos.modules.company.Entity;
 import java.time.LocalDate;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
 
 @Entity
 public class CompanyEntity {
@@ -15,12 +20,15 @@ public class CompanyEntity {
     private UUID id;
     private String username;
 
-    
+    @Size(min = 5, max = 10, message = "The [password] field have to be between 5 and 10 characteres")
     private String password;
 
-    
+    @Email(message = "The fieled email cannot be null")
     private String email;
 
+    @CreationTimestamp
     private LocalDate created_at;
+
+    @UpdateTimestamp
     private LocalDate updated_at;
 }
