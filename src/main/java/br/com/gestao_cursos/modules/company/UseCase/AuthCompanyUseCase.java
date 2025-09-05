6317e0f703d21e084eea2a52cdcd223cd74bc27e
JPSSmaths <joaopedrosilvadasilva4@gmail.com>
2025-09-05 12:52:42 -0300

    Secret definition to AuthCompany and apllcition of this in AuthCompanyUseCase

--- src/main/java/br/com/gestao_cursos/modules/company/UseCase/AuthCompanyUseCase.java
+++ src/main/java/br/com/gestao_cursos/modules/company/UseCase/AuthCompanyUseCase.java
@@ -1,11 +1,21 @@
 package br.com.gestao_cursos.modules.company.UseCase;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import br.com.gestao_cursos.modules.company.dto.AuthCompanyRequestDTO;
 
 @Service
 public class AuthCompanyUseCase {
+    @Value("${security.jwt.secret.company}")
+    private String secret;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
     
-    public void auth(){
+    public void auth(AuthCompanyRequestDTO authCompanyRequestDTO){
         
     }
 }
