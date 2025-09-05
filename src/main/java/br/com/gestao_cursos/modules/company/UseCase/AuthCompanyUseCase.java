7adeb776364ad8f190cda77099f618cbc3ccd0a4
JPSSmaths <joaopedrosilvadasilva4@gmail.com>
2025-09-05 16:57:19 -0300

    Adding new jwt dependency

--- src/main/java/br/com/gestao_cursos/modules/company/UseCase/AuthCompanyUseCase.java
+++ src/main/java/br/com/gestao_cursos/modules/company/UseCase/AuthCompanyUseCase.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import br.com.gestao_cursos.exceptions.UserFoundException;
+import br.com.gestao_cursos.modules.company.Repository.CompanyRepository;
 import br.com.gestao_cursos.modules.company.dto.AuthCompanyRequestDTO;
 
 @Service
@@ -14,8 +16,20 @@ public class AuthCompanyUseCase {
 
     @Autowired
     private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private CompanyRepository companyRepository;
     
     public void auth(AuthCompanyRequestDTO authCompanyRequestDTO){
+        var company = this.companyRepository.findByUsernameOrEmail(authCompanyRequestDTO.getUsername(), null)
+        .orElseThrow(() -> new UserFoundException("Usuário não cadastrado"));
+
+        if(!this.passwordEncoder.matches(authCompanyRequestDTO.getPassword(), company.getPassword())){
+            throw new UserFoundException("Senha inválida");
+        }
+
+        
         
     }
+
 }
