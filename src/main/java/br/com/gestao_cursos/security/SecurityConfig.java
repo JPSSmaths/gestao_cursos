2674a03e2807b041a1e6b71e7b61aefbd6a04f89
JPSSmaths <joaopedrosilvadasilva4@gmail.com>
2025-09-08 15:30:40 -0300

    SecurityCompanyFilter implementation in SecurityConfig

--- src/main/java/br/com/gestao_cursos/security/SecurityConfig.java
+++ src/main/java/br/com/gestao_cursos/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package br.com.gestao_cursos.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
@@ -7,10 +8,14 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 @EnableMethodSecurity
 public class SecurityConfig {
+
+    @Autowired
+    private SecurityCompanyFilter securityCompanyFilter;
     
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
@@ -19,6 +24,7 @@ public class SecurityConfig {
             auth.requestMatchers("/student/**").permitAll();
             auth.anyRequest().authenticated();
         });
+        http.addFilterBefore(this.securityCompanyFilter, BasicAuthenticationFilter.class);
 
         return http.build();
     }
