e2631ce86beca96293332fd2723b070d8a4a3664
JPSSmaths <joaopedrosilvadasilva4@gmail.com>
2025-09-09 08:53:17 -0300

    GetCompanyUseCase logic creation

--- src/main/java/br/com/gestao_cursos/modules/company/UseCase/GetCompanyUseCase.java
+++ src/main/java/br/com/gestao_cursos/modules/company/UseCase/GetCompanyUseCase.java
@@ -1,8 +1,12 @@
 package br.com.gestao_cursos.modules.company.UseCase;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.gestao_cursos.exceptions.UserFoundException;
+import br.com.gestao_cursos.modules.company.Entity.CompanyEntity;
 import br.com.gestao_cursos.modules.company.Repository.CompanyRepository;
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -12,7 +16,12 @@ public class GetCompanyUseCase {
     @Autowired
     private CompanyRepository companyRepository;
 
-    public void get(HttpServletRequest request){
+    public CompanyEntity get(HttpServletRequest request){
+        UUID company_id = UUID.fromString(request.getAttribute("company_id").toString());
 
+        var company = this.companyRepository.findById(company_id)
+            .orElseThrow(() -> new UserFoundException("Company not found"));
+
+        return company;
     }
 }
