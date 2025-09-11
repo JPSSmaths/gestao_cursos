472363d5f54f07eb7d6f189823a1b89f988654c0
JPSSmaths <joaopedrosilvadasilva4@gmail.com>
2025-09-11 09:33:59 -0300

    Alteration in anotation validation to valid in CursoController

--- src/main/java/br/com/gestao_cursos/modules/company/curso/Controllers/CursoController.java
+++ src/main/java/br/com/gestao_cursos/modules/company/curso/Controllers/CursoController.java
@@ -3,13 +3,13 @@ package br.com.gestao_cursos.modules.company.curso.Controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.gestao_cursos.modules.company.curso.Entity.CursoEntity;
 import br.com.gestao_cursos.modules.company.curso.UseCase.CreateCursoUseCase;
+import jakarta.validation.Valid;
 
 @RestController
 @RequestMapping("/curso")
@@ -17,7 +17,7 @@ public class CursoController {
     @Autowired
     private CreateCursoUseCase createCursoUseCase;
 
-    public ResponseEntity<Object> create(@Validated @RequestBody CursoEntity cursoEntity){
+    public ResponseEntity<Object> create(@Valid @RequestBody CursoEntity cursoEntity){
         try {
             var result = this.createCursoUseCase.execute(cursoEntity);
             return ResponseEntity.ok().body(result);
