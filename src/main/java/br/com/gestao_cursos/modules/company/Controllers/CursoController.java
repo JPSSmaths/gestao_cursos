abb333686bc2b5e096980e0f3bf92cdecdb9d479
JPSSmaths <joaopedrosilvadasilva4@gmail.com>
2025-09-13 15:29:49 -0300

    PutCursoUseCase logic creation and implementation of this in CursoController

--- src/main/java/br/com/gestao_cursos/modules/company/Controllers/CursoController.java
+++ src/main/java/br/com/gestao_cursos/modules/company/Controllers/CursoController.java
@@ -1,17 +1,22 @@
 package br.com.gestao_cursos.modules.company.Controllers;
 
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.gestao_cursos.modules.company.UseCase.CreateCursoUseCase;
 import br.com.gestao_cursos.modules.company.UseCase.GetCursoUseCase;
+import br.com.gestao_cursos.modules.company.UseCase.PutCursoUseCase;
 import br.com.gestao_cursos.modules.company.curso.Entity.CursoEntity;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
@@ -24,6 +29,9 @@ public class CursoController {
 
     @Autowired
     private GetCursoUseCase getCursoUseCase;
+
+    @Autowired
+    private PutCursoUseCase putCursoUseCase;
 
     @PostMapping("/create")
     @PreAuthorize("hasRole('COMPANY')")
@@ -44,6 +52,18 @@ public class CursoController {
             return ResponseEntity.ok().body(result);
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+        }
+    }
+
+    @PutMapping("/put/{curso_id}")
+    @PreAuthorize("hasRole('COMPANY')")
+    public ResponseEntity<Object> put(@PathVariable UUID curso_id, @RequestBody CursoEntity cursoEntity){
+        try {
+            var result = this.putCursoUseCase.execute(curso_id, cursoEntity);
+            return ResponseEntity.ok().body(result);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
 }
--- src/main/java/br/com/gestao_cursos/modules/company/UseCase/PutCursoUseCase.java
+++ src/main/java/br/com/gestao_cursos/modules/company/UseCase/PutCursoUseCase.java
@@ -1,5 +1,28 @@
 package br.com.gestao_cursos.modules.company.UseCase;
 
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.gestao_cursos.exceptions.UserFoundException;
+import br.com.gestao_cursos.modules.company.curso.Entity.CursoEntity;
+import br.com.gestao_cursos.modules.company.curso.Repository.CursoRepository;
+
+@Service
 public class PutCursoUseCase {
-    
+    @Autowired
+    private CursoRepository cursoRepository;
+
+    public CursoEntity execute(UUID curso_id, CursoEntity cursoEntity){
+        var curso = this.cursoRepository.findById(curso_id)
+            .orElseThrow(() -> {
+                throw new UserFoundException("Course not found");
+            });
+        
+        cursoEntity.setId(curso_id);
+        cursoEntity.setCompany(curso.getCompany());
+        cursoEntity.setCreated_at(curso.getCreated_at());
+        return this.cursoRepository.save(cursoEntity);
+    }
 }
