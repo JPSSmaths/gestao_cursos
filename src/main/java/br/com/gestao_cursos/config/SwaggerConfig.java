18599f46842caca093b1c360efaae68b19c21f33
JPSSmaths <joaopedrosilvadasilva4@gmail.com>
2025-09-17 15:18:52 -0300

    SwaggerConfig logic finalization

--- src/main/java/br/com/gestao_cursos/config/SwaggerConfig.java
+++ src/main/java/br/com/gestao_cursos/config/SwaggerConfig.java
@@ -1,9 +1,28 @@
 package br.com.gestao_cursos.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 
 @Configuration
 public class SwaggerConfig {
     
-    
+    @Bean
+    public OpenAPI openAPI(){
+        return new OpenAPI()
+            .info(new Info().title("Gestão de cursos").description("API responsável por gestão de cursos")
+                .version("1"))
+            .schemaRequirement("jwt_auth", createSecurityScheme());
+    }
+
+    private SecurityScheme createSecurityScheme() {
+        return new SecurityScheme()
+            .type(SecurityScheme.Type.HTTP)
+            .scheme("bearer")
+            .bearerFormat("JWT")
+            .name("jwt_auth");
+    }
 }
