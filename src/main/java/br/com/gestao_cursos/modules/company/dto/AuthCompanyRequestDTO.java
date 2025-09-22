6a431c0773e6e3e27b2caefeee3f61a8bde54d53
JPSSmaths <joaopedrosilvadasilva4@gmail.com>
2025-09-22 12:15:51 -0300

    adding examples in AuthCompanyRequestDTO

--- src/main/java/br/com/gestao_cursos/modules/company/dto/AuthCompanyRequestDTO.java
+++ src/main/java/br/com/gestao_cursos/modules/company/dto/AuthCompanyRequestDTO.java
@@ -1,11 +1,15 @@
 package br.com.gestao_cursos.modules.company.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
 public class AuthCompanyRequestDTO {
+    @Schema(example="accenture_company", requiredMode=RequiredMode.REQUIRED)
     private String username;
+    @Schema(example="accenture_123", requiredMode=RequiredMode.REQUIRED)
     private String password;
 }
