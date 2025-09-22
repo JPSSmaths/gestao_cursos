f2f0c73e242b23f30908be43682db16870d9f666
JPSSmaths <joaopedrosilvadasilva4@gmail.com>
2025-09-22 15:50:51 -0300

    Adding example for documentation in PatchCursoDTO

--- src/main/java/br/com/gestao_cursos/modules/company/curso/dto/PatchCursoDTO.java
+++ src/main/java/br/com/gestao_cursos/modules/company/curso/dto/PatchCursoDTO.java
@@ -1,5 +1,9 @@
 package br.com.gestao_cursos.modules.company.curso.dto;
 
-public record  PatchCursoDTO (String active){
-    
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
+
+public record PatchCursoDTO(
+        @Schema(example = "ACTIVE", requiredMode = RequiredMode.REQUIRED) String active) {
+
 }
