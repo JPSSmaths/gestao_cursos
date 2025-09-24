559655e4118c5d4d2cc2975f67d46288189dd599
JPSSmaths <joaopedrosilvadasilva4@gmail.com>
2025-09-24 16:14:55 -0300

    Adding assertj dependency in pom.xml and logic construction in create a company that not exist

--- src/test/java/br/com/gestao_cursos/modules/company/UseCase/CreateCompanyUseCaseTest.java
+++ src/test/java/br/com/gestao_cursos/modules/company/UseCase/CreateCompanyUseCaseTest.java
@@ -1,5 +1,7 @@
 package br.com.gestao_cursos.modules.company.UseCase;
 
+import java.util.UUID;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -7,6 +9,7 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import br.com.gestao_cursos.modules.company.Entity.CompanyEntity;
 import br.com.gestao_cursos.modules.company.Repository.CompanyRepository;
 
 @ExtendWith(MockitoExtension.class)
@@ -21,8 +24,19 @@ public class CreateCompanyUseCaseTest {
     @Test
     @DisplayName("Should not be able create a company that alredy exists")
     public void should_not_be_able_create_a_company_that_alredy_exists(){
+        var company = CompanyEntity.builder()
+            .username("USERNAME_TEST")
+            .description("DESCRIPTION_TEST")
+            .email("EMAILTEST@gmail.com")
+            .password("PASSWORD_TEST")
+            .id(UUID.randomUUID())
+            .build();
+        
+        this.companyRepository.save(company);
+
         try {
-            
+            var companyExistent = company;
+            this.companyRepository.save(companyExistent);
         } catch (Exception e) {
             
         }
