62aef14d5dadab05fdda78dbfdf473c67182eb28
JPSSmaths <joaopedrosilvadasilva4@gmail.com>
2025-10-10 15:58:24 -0300

    Should not be able list courses of a company that not exists test creation and conclusion

--- src/test/java/br/com/gestao_cursos/modules/company/UseCase/GetCursoUseCaseTest.java
+++ src/test/java/br/com/gestao_cursos/modules/company/UseCase/GetCursoUseCaseTest.java
@@ -1,10 +1,18 @@
 package br.com.gestao_cursos.modules.company.UseCase;
 
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import static org.mockito.Mockito.when;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import br.com.gestao_cursos.exceptions.CompanyNotFoundException;
 import br.com.gestao_cursos.modules.company.curso.Repository.CursoRepository;
 
 @ExtendWith(MockitoExtension.class)
@@ -16,7 +24,17 @@ public class GetCursoUseCaseTest {
     @Mock
     private CursoRepository cursoRepository;
 
-    
+    @Test
+    @DisplayName("Should not be able list courses of a company that not exists")
+    public void should_not_be_able_list_courses_of_a_company_that_not_exists(){
+        UUID companyId = UUID.randomUUID();
+
+        when(this.cursoRepository.findAllByCompanyId(companyId)).thenReturn(Optional.empty());
+
+        assertThatThrownBy(() -> {
+            this.getCursoUseCase.execute(companyId);
+        }).isInstanceOf(CompanyNotFoundException.class);
+    }
 
 
 }
