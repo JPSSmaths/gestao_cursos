ac6889204ea19136a32729aed27db177c74360af
JPSSmaths <joaopedrosilvadasilva4@gmail.com>
2025-10-10 15:43:04 -0300

    Changing CursoNotFoundException to CompanyNotFoundException in GetCursoUseCase

--- src/main/java/br/com/gestao_cursos/modules/company/UseCase/GetCursoUseCase.java
+++ src/main/java/br/com/gestao_cursos/modules/company/UseCase/GetCursoUseCase.java
@@ -6,7 +6,7 @@ import java.util.UUID;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import br.com.gestao_cursos.exceptions.CursoNotFoundException;
+import br.com.gestao_cursos.exceptions.CompanyNotFoundException;
 import br.com.gestao_cursos.modules.company.curso.Repository.CursoRepository;
 
 @Service
@@ -17,7 +17,7 @@ public class GetCursoUseCase {
     public List execute(UUID companyId){
         List cursos = this.cursoRepository.findAllByCompanyId(companyId)
             .orElseThrow(() -> {
-                throw new CursoNotFoundException();
+                throw new CompanyNotFoundException();
             });
         
         return cursos;
