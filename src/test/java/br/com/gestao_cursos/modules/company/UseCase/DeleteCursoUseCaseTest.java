d93288171864450221f69e9057a89d44bcc7d5e4
JPSSmaths <joaopedrosilvadasilva4@gmail.com>
2025-10-12 20:03:47 -0300

    DeleteCursoUseCaseTest creation

---
+++ src/test/java/br/com/gestao_cursos/modules/company/UseCase/DeleteCursoUseCaseTest.java
@@ -0,0 +1,19 @@
+package br.com.gestao_cursos.modules.company.UseCase;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import br.com.gestao_cursos.modules.company.curso.Repository.CursoRepository;
+
+@ExtendWith(MockitoExtension.class)
+public class DeleteCursoUseCaseTest {
+    @InjectMocks
+    private DeleteCursoUseCase deleteCursoUseCase;
+
+    @Mock
+    private CursoRepository cursoRepository;
+
+    
+}
