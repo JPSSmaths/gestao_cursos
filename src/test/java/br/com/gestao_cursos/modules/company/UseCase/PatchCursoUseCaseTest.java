aef3011d097896da946a916b5abd68a53394a930
JPSSmaths <joaopedrosilvadasilva4@gmail.com>
2025-10-12 19:41:52 -0300

    PatchCursoUseCaseTest creation

---
+++ src/test/java/br/com/gestao_cursos/modules/company/UseCase/PatchCursoUseCaseTest.java
@@ -0,0 +1,19 @@
+package br.com.gestao_cursos.modules.company.UseCase;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import br.com.gestao_cursos.modules.company.curso.Repository.CursoRepository;
+
+@ExtendWith(MockitoExtension.class)
+public class PatchCursoUseCaseTest {
+    @InjectMocks
+    PatchCursoUseCase patchCursoUseCase;
+
+    @Mock
+    CursoRepository cursoRepository;
+
+    
+}
