2b371241e9c1f4436a12118d9480163ca2dd6d84
JPSSmaths <joaopedrosilvadasilva4@gmail.com>
2025-10-12 19:35:13 -0300

    Should be possible to update a existent course test creation and logic finalization

--- src/test/java/br/com/gestao_cursos/modules/company/UseCase/PutCursoUseCaseTest.java
+++ src/test/java/br/com/gestao_cursos/modules/company/UseCase/PutCursoUseCaseTest.java
@@ -1,8 +1,10 @@
 package br.com.gestao_cursos.modules.company.UseCase;
 
 import java.util.Optional;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,7 +14,10 @@ import static org.mockito.Mockito.when;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import br.com.gestao_cursos.exceptions.CursoNotFoundException;
+import br.com.gestao_cursos.modules.company.curso.Active;
+import br.com.gestao_cursos.modules.company.curso.Entity.CursoEntity;
 import br.com.gestao_cursos.modules.company.curso.Repository.CursoRepository;
+import br.com.gestao_cursos.modules.company.curso.dto.PutCursoDTO;
 
 @ExtendWith(MockitoExtension.class)
 public class PutCursoUseCaseTest {
@@ -25,10 +30,39 @@ public class PutCursoUseCaseTest {
     @Test
     @DisplayName("Should not be possible to update a non-existent course")
     public void should_not_be_possible_to_update_a_non_existent_course() {
-        when(this.cursoRepository.findById(null)).thenReturn(Optional.empty());
+        var cursoId = UUID.randomUUID();
+        when(this.cursoRepository.findById(cursoId)).thenReturn(Optional.empty());
 
         assertThatThrownBy(() -> {
-            this.putCursoUseCase.execute(null, null);
+            this.putCursoUseCase.execute(cursoId, null);
         }).isInstanceOf(CursoNotFoundException.class);
+    }
+
+    @Test
+    @DisplayName("Should be possible to update a existent course")
+    public void should_be_possible_to_update_a_existent_course() {
+        PutCursoDTO putCursoDTO = PutCursoDTO.builder()
+                .name("COURSE_TEST_UPDATED")
+                .category("CATEGORY_TEST_UPDATED")
+                .active(Enum.valueOf(Active.class, "ACTIVE"))
+                .build();
+        
+        CursoEntity course = CursoEntity.builder()
+                .id(UUID.randomUUID())
+                .name(putCursoDTO.getName())
+                .category(putCursoDTO.getCategory())
+                .active(putCursoDTO.getActive())
+                .build();
+
+        CursoEntity courseUpdated = course;
+        courseUpdated.setName("TEST_NAME");
+        courseUpdated.setCategory("CATEGORY_TEST");
+        courseUpdated.setActive(Enum.valueOf(Active.class, "INACTIVE"));
+
+        when(this.cursoRepository.findById(course.getId())).thenReturn(Optional.of(course));
+        when(this.cursoRepository.save(course)).thenReturn(courseUpdated);
+
+        var result = this.putCursoUseCase.execute(course.getId(), putCursoDTO);
+        assertEquals(courseUpdated, result);
     }
 }
