a4f8f809df56fd6b9868109455b9057ac0596c5e
JPSSmaths <joaopedrosilvadasilva4@gmail.com>
2025-10-12 19:12:12 -0300

    Should not be possible to update a non-existent course test crearion and logic finalization

--- src/test/java/br/com/gestao_cursos/modules/company/UseCase/PutCursoUseCaseTest.java
+++ src/test/java/br/com/gestao_cursos/modules/company/UseCase/PutCursoUseCaseTest.java
@@ -1,10 +1,17 @@
 package br.com.gestao_cursos.modules.company.UseCase;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import static org.mockito.Mockito.when;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import br.com.gestao_cursos.exceptions.CursoNotFoundException;
 import br.com.gestao_cursos.modules.company.curso.Repository.CursoRepository;
 
 @ExtendWith(MockitoExtension.class)
@@ -15,5 +22,13 @@ public class PutCursoUseCaseTest {
     @Mock
     private CursoRepository cursoRepository;
 
-    
+    @Test
+    @DisplayName("Should not be possible to update a non-existent course")
+    public void should_not_be_possible_to_update_a_non_existent_course() {
+        when(this.cursoRepository.findById(null)).thenReturn(Optional.empty());
+
+        assertThatThrownBy(() -> {
+            this.putCursoUseCase.execute(null, null);
+        }).isInstanceOf(CursoNotFoundException.class);
+    }
 }
