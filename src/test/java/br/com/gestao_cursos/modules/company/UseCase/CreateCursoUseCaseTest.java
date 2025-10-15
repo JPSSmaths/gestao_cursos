7122c7381dbc254bfe30d8c9344b6f9cd01418e5
JPSSmaths <joaopedrosilvadasilva4@gmail.com>
2025-10-15 09:45:18 -0300

    Adding CompanyEntitiy how mock and Should not able to create a course if a company does not exist creation and logic finalization

--- src/test/java/br/com/gestao_cursos/modules/company/UseCase/CreateCursoUseCaseTest.java
+++ src/test/java/br/com/gestao_cursos/modules/company/UseCase/CreateCursoUseCaseTest.java
@@ -1,6 +1,7 @@
 package br.com.gestao_cursos.modules.company.UseCase;
 
 import java.util.Optional;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import org.junit.jupiter.api.DisplayName;
@@ -11,8 +12,10 @@ import org.mockito.Mock;
 import static org.mockito.Mockito.when;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import br.com.gestao_cursos.exceptions.CompanyNotFoundException;
 import br.com.gestao_cursos.exceptions.CursoAlreadyExistsException;
 import br.com.gestao_cursos.modules.company.Entity.CompanyEntity;
+import br.com.gestao_cursos.modules.company.Repository.CompanyRepository;
 import br.com.gestao_cursos.modules.company.curso.Entity.CursoEntity;
 import br.com.gestao_cursos.modules.company.curso.Repository.CursoRepository;
 import br.com.gestao_cursos.modules.company.curso.dto.CreateCursoDTO;
@@ -24,6 +27,9 @@ public class CreateCursoUseCaseTest {
 
     @Mock
     private CursoRepository cursoRepository;
+
+    @Mock
+    private CompanyRepository companyRepository;
 
     @Test
     @DisplayName("Should not able to create a new course when curso already exists")
@@ -45,4 +51,23 @@ public class CreateCursoUseCaseTest {
         assertThatThrownBy(() -> this.createCursoUseCase.execute(createCursoDTO, companyEntity.getId()))
                 .isInstanceOf(CursoAlreadyExistsException.class);
     }
+
+    @Test
+    @DisplayName("Should not able to create a course if a company does not exist")
+    public void should_not_be_able_to_create_a_course_if_a_company_does_not_exist() {
+        CreateCursoDTO createCursoDTO = CreateCursoDTO.builder()
+                .name("Java")
+                .build();
+
+        CompanyEntity companyEntity = CompanyEntity.builder()
+                .id(UUID.randomUUID())
+                .build();
+
+        when(this.companyRepository.findById(companyEntity.getId())).thenReturn(Optional.empty());
+
+        assertThatThrownBy(() -> this.createCursoUseCase.execute(createCursoDTO, companyEntity.getId()))
+                .isInstanceOf(CompanyNotFoundException.class);
+    }
+
+
 }
