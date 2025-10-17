60f5f87beae26d04978b36eddaadfb540471db01
JPSSmaths <joaopedrosilvadasilva4@gmail.com>
2025-10-17 18:46:12 -0300

    Should not be able to generate a token with a non existent company test logic conclusion

--- src/main/java/br/com/gestao_cursos/modules/company/dto/AuthCompanyRequestDTO.java
+++ src/main/java/br/com/gestao_cursos/modules/company/dto/AuthCompanyRequestDTO.java
@@ -3,10 +3,12 @@ package br.com.gestao_cursos.modules.company.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.media.Schema.RequiredMode;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
 @AllArgsConstructor
+@Builder
 public class AuthCompanyRequestDTO {
     @Schema(example="accenture_company", requiredMode=RequiredMode.REQUIRED)
     private String username;
--- src/test/java/br/com/gestao_cursos/modules/company/UseCase/AuthCompanyUseCaseTest.java
+++ src/test/java/br/com/gestao_cursos/modules/company/UseCase/AuthCompanyUseCaseTest.java
@@ -1,14 +1,22 @@
 package br.com.gestao_cursos.modules.company.UseCase;
 
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import static org.mockito.Mockito.when;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import br.com.gestao_cursos.exceptions.CompanyNotFoundException;
+import br.com.gestao_cursos.modules.company.Entity.CompanyEntity;
 import br.com.gestao_cursos.modules.company.Repository.CompanyRepository;
+import br.com.gestao_cursos.modules.company.dto.AuthCompanyRequestDTO;
 
 @ExtendWith(MockitoExtension.class)
 public class AuthCompanyUseCaseTest {
@@ -22,8 +30,23 @@ public class AuthCompanyUseCaseTest {
     private CompanyRepository companyRepository;
 
     @Test
-    @DisplayName("Should be able to generate token with a valid company")
+    @DisplayName("Should not be able to generate token with a valid company")
     public void should_not_be_able_to_generate_token_with_a_non_existent_company(){
+        var authCompanyRequestDTO = AuthCompanyRequestDTO.builder()
+            .username("COMPANY_TEST")
+            .password("PASSWORD_TEST")
+            .build();
+
+        CompanyEntity company = CompanyEntity.builder()
+            .id(UUID.randomUUID())
+            .username(authCompanyRequestDTO.getUsername())
+            .password(authCompanyRequestDTO.getPassword())
+            .build();
         
+        when(this.companyRepository.findByUsernameOrEmail(company.getUsername(), null))
+            .thenReturn(Optional.empty());
+
+        assertThatThrownBy(() -> this.authCompanyUseCase.auth(authCompanyRequestDTO))
+            .isInstanceOf(CompanyNotFoundException.class);
     }
 }
