14a59603a23c78dcd724c01aec1a376e210350b8
JPSSmaths <joaopedrosilvadasilva4@gmail.com>
2025-10-18 16:12:29 -0300

    junit dependency insertion

--- src/test/java/br/com/gestao_cursos/modules/company/controllers/CreateCompanyControllerTest.java
+++ src/test/java/br/com/gestao_cursos/modules/company/controllers/CreateCompanyControllerTest.java
@@ -1,6 +1,9 @@
 package br.com.gestao_cursos.modules.company.controllers;
 
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 public class CreateCompanyControllerTest {
     
 }
