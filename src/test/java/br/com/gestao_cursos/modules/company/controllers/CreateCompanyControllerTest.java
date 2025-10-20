f29766456fc681d102b0af67a3183da4650bca5b
JPSSmaths <joaopedrosilvadasilva4@gmail.com>
2025-10-20 12:01:04 -0300

    Creating configurations tests in CreateCompanyControllerTest to integration test

--- src/test/java/br/com/gestao_cursos/modules/company/controllers/CreateCompanyControllerTest.java
+++ src/test/java/br/com/gestao_cursos/modules/company/controllers/CreateCompanyControllerTest.java
@@ -1,9 +1,45 @@
 package br.com.gestao_cursos.modules.company.controllers;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import br.com.gestao_cursos.modules.company.Repository.CompanyRepository;
 
 @RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 public class CreateCompanyControllerTest {
     
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    @Before
+    void setUp(){
+        this.mockMvc = MockMvcBuilders
+                        .webAppContextSetup(this.webApplicationContext)
+                        .apply(SecurityMockMvcConfigurers.springSecurity())
+                        .build();
+    }
+
+    @Test
+    @DisplayName("Should be able to create a new company")
+    public void should_be_able_to_create_a_new_company(){
+        
+    }
 }
