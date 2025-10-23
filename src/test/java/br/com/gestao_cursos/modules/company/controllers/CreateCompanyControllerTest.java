085543c65acd2c5e6bf4f5c6f1d5d1c76e27a769
JPSSmaths <joaopedrosilvadasilva4@gmail.com>
2025-10-22 21:38:45 -0300

    Should be able to create a new company integration finalization and objectToJSON method creation in JWTCompanyProviderTest

--- src/test/java/br/com/gestao_cursos/modules/company/controllers/CreateCompanyControllerTest.java
+++ src/test/java/br/com/gestao_cursos/modules/company/controllers/CreateCompanyControllerTest.java
@@ -7,14 +7,18 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import br.com.gestao_cursos.modules.company.Repository.CompanyRepository;
+import br.com.gestao_cursos.modules.company.dto.CreateCompanyDTO;
+import br.com.gestao_cursos.modules.company.utils.JWTCompanyProviderTest;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
@@ -30,16 +34,26 @@ public class CreateCompanyControllerTest {
     private CompanyRepository companyRepository;
 
     @Before
-    void setUp(){
+    public void setUp(){
         this.mockMvc = MockMvcBuilders
                         .webAppContextSetup(this.webApplicationContext)
-                        .apply(SecurityMockMvcConfigurers.springSecurity())
                         .build();
     }
 
     @Test
     @DisplayName("Should be able to create a new company")
-    public void should_be_able_to_create_a_new_company(){
+    public void should_be_able_to_create_a_new_company() throws Exception{
+        var companyDTO = CreateCompanyDTO.builder()
+                        .username("COMPANY_TEST")
+                        .description("DESCRIPTION_TEST")
+                        .email("company@gmail.com")
+                        .password("1234567890")
+                        .build();
         
+        var result = this.mockMvc.perform(
+            MockMvcRequestBuilders.post("/company/create")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(JWTCompanyProviderTest.objectToJSON(companyDTO))
+        ).andExpect(MockMvcResultMatchers.status().isCreated());
     }
 }
