5c4700cbaa66c545237b545a66045b63a4c6fc4f
JPSSmaths <joaopedrosilvadasilva4@gmail.com>
2025-10-23 17:09:53 -0300

    Should not be able to create a new company with existing username integration test creation and logic finalization

--- src/test/java/br/com/gestao_cursos/modules/company/controllers/CreateCompanyControllerTest.java
+++ src/test/java/br/com/gestao_cursos/modules/company/controllers/CreateCompanyControllerTest.java
@@ -16,6 +16,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import br.com.gestao_cursos.modules.company.Entity.CompanyEntity;
 import br.com.gestao_cursos.modules.company.Repository.CompanyRepository;
 import br.com.gestao_cursos.modules.company.dto.CreateCompanyDTO;
 import br.com.gestao_cursos.modules.company.utils.JWTCompanyProviderTest;
@@ -24,36 +25,63 @@ import br.com.gestao_cursos.modules.company.utils.JWTCompanyProviderTest;
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @ActiveProfiles("test")
 public class CreateCompanyControllerTest {
-    
-    private MockMvc mockMvc;
 
-    @Autowired
-    private WebApplicationContext webApplicationContext;
+        private MockMvc mockMvc;
 
-    @Autowired
-    private CompanyRepository companyRepository;
+        @Autowired
+        private WebApplicationContext webApplicationContext;
 
-    @Before
-    public void setUp(){
-        this.mockMvc = MockMvcBuilders
-                        .webAppContextSetup(this.webApplicationContext)
-                        .build();
-    }
+        @Autowired
+        private CompanyRepository companyRepository;
 
-    @Test
-    @DisplayName("Should be able to create a new company")
-    public void should_be_able_to_create_a_new_company() throws Exception{
-        var companyDTO = CreateCompanyDTO.builder()
-                        .username("COMPANY_TEST")
-                        .description("DESCRIPTION_TEST")
-                        .email("company@gmail.com")
-                        .password("1234567890")
-                        .build();
-        
-        var result = this.mockMvc.perform(
-            MockMvcRequestBuilders.post("/company/create")
-            .contentType(MediaType.APPLICATION_JSON)
-            .content(JWTCompanyProviderTest.objectToJSON(companyDTO))
-        ).andExpect(MockMvcResultMatchers.status().isCreated());
-    }
+        @Before
+        public void setUp() {
+                this.mockMvc = MockMvcBuilders
+                                .webAppContextSetup(this.webApplicationContext)
+                                .build();
+        }
+
+        @Test
+        @DisplayName("Should be able to create a new company")
+        public void should_be_able_to_create_a_new_company() throws Exception {
+                var companyDTO = CreateCompanyDTO.builder()
+                                .username("COMPANY_TEST")
+                                .description("DESCRIPTION_TEST")
+                                .email("company@gmail.com")
+                                .password("1234567890")
+                                .build();
+
+                var result = this.mockMvc.perform(
+                                MockMvcRequestBuilders.post("/company/create")
+                                                .contentType(MediaType.APPLICATION_JSON)
+                                                .content(JWTCompanyProviderTest.objectToJSON(companyDTO)))
+                                .andExpect(MockMvcResultMatchers.status().isCreated());
+        }
+
+        @Test
+        @DisplayName("Should not be able to create a new company with existing username")
+        public void should_not_be_able_to_create_a_new_company_with_existing_username() throws Exception {
+                var companyDTO = CreateCompanyDTO.builder()
+                                .username("COMPANY_TEST")
+                                .description("DESCRIPTION_TEST")
+                                .email("email@gmail.com")
+                                .password("1234567890")
+                                .build();
+
+                var existingCompany = CompanyEntity.builder()
+                                .username("COMPANY_TEST")
+                                .description("EXISTING_DESCRIPTION")
+                                .email("existing@email.com")
+                                .password("hashed_password")
+                                .build();
+
+                companyRepository.saveAndFlush(existingCompany);
+
+                var result = this.mockMvc.perform(
+                                MockMvcRequestBuilders.post("/company/create")
+                                                .contentType(MediaType.APPLICATION_JSON)
+                                                .content(JWTCompanyProviderTest.objectToJSON(companyDTO)))
+                                .andExpect(MockMvcResultMatchers.status().isBadRequest());
+        }
+
 }
