1baf901860318ad03e1a36f68738d9d8ad79ea30
JPSSmaths <joaopedrosilvadasilva4@gmail.com>
2025-11-03 13:47:55 -0300

    DeleteCursoUseCase creation

---
+++ src/test/java/br/com/gestao_cursos/modules/company/controllers/DeleteCursoControllerTest.java
@@ -0,0 +1,45 @@
+package br.com.gestao_cursos.modules.company.controllers;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import br.com.gestao_cursos.modules.company.Repository.CompanyRepository;
+import br.com.gestao_cursos.modules.company.curso.Repository.CursoRepository;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+public class DeleteCursoControllerTest {
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Autowired
+    private CursoRepository cursoRepository;
+
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    @Before
+    public void setUp() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .apply(SecurityMockMvcConfigurers.springSecurity()).build();
+    }
+
+    @Test
+    @DisplayName("")
+    public void should_be_able_detete_a_course_of_a_existent_company(){
+        
+    }
+}
