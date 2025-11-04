465e27a6a3a179fa09e7d6b0257c1de620d09102
JPSSmaths <joaopedrosilvadasilva4@gmail.com>
2025-11-04 08:02:55 -0300

    PutCursoController creation

---
+++ src/test/java/br/com/gestao_cursos/modules/company/controllers/PutCursoControllerTest.java
@@ -0,0 +1,40 @@
+package br.com.gestao_cursos.modules.company.controllers;
+
+import org.junit.Before;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import br.com.gestao_cursos.modules.company.Repository.CompanyRepository;
+import br.com.gestao_cursos.modules.company.curso.Repository.CursoRepository;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
+public class PutCursoControllerTest {
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Autowired
+    private CursoRepository cursoRepository;
+
+    @Autowired
+    private CompanyRepository companyRepository;
+
+    @Before
+    public void setUp(){
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .apply(SecurityMockMvcConfigurers.springSecurity()).build();
+    }
+
+    
+}
