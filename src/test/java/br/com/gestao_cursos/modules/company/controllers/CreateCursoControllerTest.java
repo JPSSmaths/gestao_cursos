4c5d958492d99a98176e9c3651be2bff45abe294
JPSSmaths <joaopedrosilvadasilva4@gmail.com>
2025-11-13 11:45:15 -0300

    Should not be able create a course without authentication token and Should not be able create a course with a invalid token integration test creation and logic finalization

--- src/test/java/br/com/gestao_cursos/modules/company/controllers/CreateCursoControllerTest.java
+++ src/test/java/br/com/gestao_cursos/modules/company/controllers/CreateCursoControllerTest.java
@@ -1,6 +1,8 @@
 package br.com.gestao_cursos.modules.company.controllers;
 
 import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,6 +11,8 @@ import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfig
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -34,6 +38,23 @@ public class CreateCursoControllerTest {
     public void setUp(){
         this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
                 .apply(SecurityMockMvcConfigurers.springSecurity()).build();
+    }
+
+    @Test
+    @DisplayName("Should not be able create a course without authentication token")
+    public void should_not_be_able_create_a_course_without_authentication_token() throws Exception{
+        this.mockMvc.perform(
+            MockMvcRequestBuilders.post("/company/course/create")
+        ).andExpect(MockMvcResultMatchers.status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("Should not be able create a course with a invalid token")
+    public void should_not_be_able_create_a_course_with_a_invalid_token() throws Exception{
+        this.mockMvc.perform(
+            MockMvcRequestBuilders.post("/company/course/create")
+            .header("Authorization", "INVALID TOKEN VALUE")
+        ).andExpect(MockMvcResultMatchers.status().isUnauthorized());
     }
 
     
