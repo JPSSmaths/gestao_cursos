46039c34dce3530b5f2b809ea75993f0c2110801
JPSSmaths <joaopedrosilvadasilva4@gmail.com>
2025-11-13 13:47:14 -0300

    Should not be able generate a token if the company don't exists integration test creation and logic finalization

--- src/test/java/br/com/gestao_cursos/modules/company/controllers/AuthCompanyControllerTest.java
+++ src/test/java/br/com/gestao_cursos/modules/company/controllers/AuthCompanyControllerTest.java
@@ -1,17 +1,25 @@
 package br.com.gestao_cursos.modules.company.controllers;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 import br.com.gestao_cursos.modules.company.Repository.CompanyRepository;
+import br.com.gestao_cursos.modules.company.dto.AuthCompanyRequestDTO;
+import br.com.gestao_cursos.modules.company.utils.TestUtils;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
@@ -25,9 +33,25 @@ public class AuthCompanyControllerTest {
     @Autowired
     private CompanyRepository companyRepository;
 
+    @Before
     public void setUp(){
         this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
                 .apply(SecurityMockMvcConfigurers.springSecurity()).build();
+    }
+
+    @Test
+    @DisplayName("Should not be able generate a token if the company don't exists")
+    public void shoul_not_be_able_generate_a_token_if_the_company_dont_exists() throws Exception{
+        AuthCompanyRequestDTO  authCompanyRequestDTO = AuthCompanyRequestDTO.builder()
+            .username("nonexistent")
+            .password("invalidpassword")
+            .build();
+
+        this.mockMvc.perform(
+            MockMvcRequestBuilders.post("/company/auth/create")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(TestUtils.objectToJSON(authCompanyRequestDTO))
+        ).andExpect(MockMvcResultMatchers.status().isBadRequest());
     }
 
     
